89685e449ca6fe3eef4d17166133c6d4a9634d1d
Timothee Cour <timothee.cour2@gmail.com>
2019-01-01 14:16:29 -0800

    simple.d: add option to skip IO to avoid messing measurements with irrelevant IO

--- simple.d
+++ simple.d
@@ -1,17 +1,21 @@
 import core.stdc.stdio: printf;
-
 int main() {
+    int t=0;
 
     int i = 0;
     for (int z = 1; ; ++z)
         for (int x = 1; x <= z; ++x)
             for (int y = x; y <= z; ++y)
                 if (x*x + y*y == z*z) {
-                    printf("(%i, %i, %i)\n", x, y, z);
+                    version(skip_printfs)
+                        t+=(x+y+z);
+                    else
+                        printf("(%i, %i, %i)\n", x, y, z);
                     if (++i == 1000)
                         goto done;
                 }
     done:
-
+    version(skip_printfs)
+        printf("%i\n", t);
     return 0;
 }
